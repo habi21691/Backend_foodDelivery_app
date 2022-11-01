cc9565bcf49d5e63db8ae7f130655c8c07dc10dc
habi <habtemariamshumye46@gmail.com>
2022-11-01 22:40:10 +0300

    commit

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ require("dotenv").config();
 
 const helmet = require("helmet");
 
+// const server = http.createServer((req, res) => {
+//   // if (req.url === "/api/feachingOrder" && req.method === "GET") {
+//   //   res.setHeader("Content-Type", "aplication/json");
+//   // res.setHeader("Access-Control-Allow-Origin", "*");
+//   //   res.write(JSON.stringify({ message: "Hello World" }));
+//   //   res.end('hello');
+//   // }
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "aplication/json");
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.end("Hello World! I am your new NodeJS app! \n");
+// });
 
 const cors = require("cors");
 
@@ -21,19 +33,25 @@ app.use("upload", express.static("upload"));
 app.use(express.json());
 app.use(cors());
 app.use("/api", routeUrls);
-const server = http.createServer((routeUrls));
 
-// app.use(
-//   helmet.contentSecurityPolicy({
-//     useDefaults: false,
-//     directives: {
-//       defaultSrc: ["'self'"],
-//       scriptSrc: ["'self'", "https://mernfood-delivery.onrender.com"],
-//       objectSrc: ["'none'"],
-//       upgradeInsecureRequests: [],
-//     },
-//   })
-// );
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: false,
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "https://mernfood-delivery.onrender.com"],
+      objectSrc: ["'none'"],
+      upgradeInsecureRequests: [],
+    },
+  })
+);
+
+const server = http.createServer( function app(req, res)  {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "aplication/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.end("Hello World! I am your new NodeJS app! \n");
+});
 
 mongoss.connect(
   "mongodb+srv://habtemariam:vZByrZCsG8LaKXUT@mern.2e1gmj9.mongodb.net/Food_Delivery?retryWrites=true&w=majority",
