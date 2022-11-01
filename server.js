82350d975858cf98ccfb4ef6b07ce7c8639ea76d
habi <habtemariamshumye46@gmail.com>
2022-11-01 23:20:01 +0300

    c

--- server.js
+++ server.js
@@ -1,48 +1,35 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 const http = require("http");
 const mongoss = require("mongoose");
 const bodyParse = require("body-parser");
 const path = require("path");
-require("dotenv").config();
 
+require("dotenv").config();
 
 const helmet = require("helmet");
 
-const server = http.createServer(app)
- 
-  // res.statusCode = 200;
-  // res.setHeader("Content-Type", "aplication/json");
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.end("Hello World! I am your new NodeJS app! \n");
+const server = http.createServer(app);
+
+// res.statusCode = 200;
+// res.setHeader("Content-Type", "aplication/json");
+// res.setHeader("Access-Control-Allow-Origin", "*");
+// res.end("Hello World! I am your new NodeJS app! \n");
 
 const cors = require("cors");
 
 const routeUrls = require("./router/route");
-const router = require("./router/route");
-const { route } = require("./router/route");
 
-// app.use(helmet());
 app.use(bodyParse.json());
 app.use(bodyParse.urlencoded({ extended: false }));
 app.use("upload", express.static("upload"));
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: 'https://mernfood-delivery.onrender.com/'}));
 app.use("/api", routeUrls);
 
-app.use(
-  helmet.contentSecurityPolicy({
-    useDefaults: false,
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "https://mernfood-delivery.onrender.com"],
-      objectSrc: ["'none'"],
-      upgradeInsecureRequests: [],
-    },
-  })
-);
-
+app.use(helmet);
 
 mongoss.connect(
   "mongodb+srv://habtemariam:vZByrZCsG8LaKXUT@mern.2e1gmj9.mongodb.net/Food_Delivery?retryWrites=true&w=majority",
