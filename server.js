af746a39869a87591954bbba3e1d3372dd62d655
habi <habtemariamshumye46@gmail.com>
2022-11-01 14:44:23 +0300

    commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParse=require('body-parser')
 const path=require('path')
 require('dotenv').config()
 
+const helmet = require("helmet");
 
 
 
@@ -26,6 +27,16 @@ app.use(cors())
 app.use('/api',routeUrls)
 
 
-
+app.use(
+    helmet.contentSecurityPolicy({
+      useDefaults: false,
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "https://mernfood-delivery.onrender.com"],
+        objectSrc: ["'none'"],
+        upgradeInsecureRequests: [],
+      },
+    })
+  );
 
 mongoss.connect('mongodb+srv://habtemariam:vZByrZCsG8LaKXUT@mern.2e1gmj9.mongodb.net/Food_Delivery?retryWrites=true&w=majority',()=>app.listen(process.env.PORT || 5000,console.log("server is up and runnig")))
