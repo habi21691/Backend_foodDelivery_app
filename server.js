7c55d65d1d3975d80dc1d676c27b2b72b175aac8
habi <habtemariamshumye46@gmail.com>
2022-11-02 21:50:06 +0300

    c

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.use(middleware.errorHandler);
 
 const server = http.createServer(app);
 mongoss.connect(
-  "mongodb+srv://habtemariam:vZByrZCsG8LaKXUT@mern.2e1gmj9.mongodb.net/Food_Delivery?retryWrites=true&w=majority",
+  "mongodb+srv://habtemariam:vZByrZCsG8LaKXUT@mern.2e1gmj9.mongodb.net/Foood_Delivery?retryWrites=true&w=majority",
   () =>
     server.listen(
       process.env.PORT || 5000,
