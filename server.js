6e15e35408da468036ad9c59cc5e767036b3436e
habi <habtemariamshumye46@gmail.com>
2022-11-04 14:20:30 +0300

    cd

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const cors = require("cors");
 // const favicon = require('express-favicon');
 
 // app.get(favicon(__dirname + '/public/favicon.ico'));
-app.use('/favicon.ico', express.static('public/favicon.ico'));
-app.get('/',express.static('/index.html'))
+app.use(favicon(path.join(__dirname + '/public/favicon.ico')))
+// app.get('/',express.static('/index.html'))
 
 const helmet = require("helmet");
 
